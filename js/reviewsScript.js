b6660e4b76d628852261c499bb923fc1101d3e0f
JongTTing <ejh990521@gmail.com>
2022-12-22 00:34:38 +0900

    no message

--- js/reviewsScript.js
+++ js/reviewsScript.js
@@ -1,7 +1,3 @@
-let username = localStorage.getItem("username");
-let userReviewProfile_img;
-let userReviewNickname;
-
 function showListSort(type) {
   $("#trip_card").empty();
   $.ajax({
@@ -16,9 +12,14 @@ function showListSort(type) {
         let userReviewFile = response[i]["reviewImgUrl"];
         let userReviewDate = response[i]["createdAt"];
         let userReviewLikes = response[i]["likeCnt"];
-        getUser(username);
-
-        let temp_html = `<li style="margin: 0 10px; height: 300px;">
+        $.ajax({
+          type: "GET",
+          url: `http://localhost:5081/user/${response[i]["username"]}`,
+          contentType: "application/json; charset=utf-8",
+          success: function (response) {
+            let userReviewProfile_img = response["profileImgUrl"];
+            let userReviewNickname = response["nickname"];
+            let temp_html = `<li style="margin: 0 10px; height: 300px;">
                                         <a onclick="moveTripDetail(${userReviewId})" class="card">
                                             <img src="${userReviewFile}" class="card__image" alt="사용자가 올린 여행지 사진"/>
                                             <div class="card__overlay">
@@ -27,7 +28,7 @@ function showListSort(type) {
                                                         <path/>
                                                     </svg>
                                                     <div class="card__thumb2">
-                                                        <img src="${userReviewProfile_img}" alt="프로필 사진"/>
+                                                        <img src="${userReviewProfile_img}" id="${i}p" alt="프로필 사진"/>
                                                     </div>
                                                     <div class="card__header-text">
                                                         <h3 class="card__title">${userReviewTitle}</h3>
@@ -40,7 +41,9 @@ function showListSort(type) {
                                             </div>
                                         </a>
                                     </li>`;
-        $("#trip_card").append(temp_html);
+            $("#trip_card").append(temp_html);
+          },
+        });
       }
     },
     error: function (error) {
@@ -56,31 +59,38 @@ function showListSort(type) {
             let userReviewFile = response[i]["reviewImgUrl"];
             let userReviewDate = response[i]["createdAt"];
             let userReviewLikes = response[i]["likeCnt"];
-            getUser(username);
-
-            let temp_html = `<li style="margin: 0 10px; height: 300px;">
-                                                <a onclick="moveTripDetail(${userReviewId})" class="card">
-                                                    <img src="${userReviewFile}" class="card__image" alt="사용자가 올린 여행지 사진"/>
-                                                    <div class="card__overlay">
-                                                        <div class="card__header">
-                                                            <svg class="card__arc" xmlns="https://www.w3.org/TR/2018/CR-SVG2-20181004/">
-                                                                <path/>
-                                                            </svg>
-                                                            <div class="card__thumb2">
-                                                                <img src="${userReviewProfile_img}" alt="프로필 사진"/>
-                                                            </div>
-                                                            <div class="card__header-text">
-                                                                <h3 class="card__title">${userReviewTitle}</h3>
-                                                                <i class="far fa-thumbs-up">${userReviewLikes}</i>
-                                                                <span class="card__status">${userReviewDate}</span>
-                                                            </div>
-                                                        </div>
-                                                        <p class="card__description">${userReviewPlace}</p>
-                                                        <p class="card__description">by <b>@${userReviewNickname}</b></p>
-                                                    </div>
-                                                </a>
-                                            </li>`;
-            $("#trip_card").append(temp_html);
+            $.ajax({
+              type: "GET",
+              url: `http://localhost:5081/user/${response[i]["username"]}`,
+              contentType: "application/json; charset=utf-8",
+              success: function (response) {
+                let userReviewProfile_img = response["profileImgUrl"];
+                let userReviewNickname = response["nickname"];
+                let temp_html = `<li style="margin: 0 10px; height: 300px;">
+                                              <a onclick="moveTripDetail(${userReviewId})" class="card">
+                                                  <img src="${userReviewFile}" class="card__image" alt="사용자가 올린 여행지 사진"/>
+                                                  <div class="card__overlay">
+                                                      <div class="card__header">
+                                                          <svg class="card__arc" xmlns="https://www.w3.org/TR/2018/CR-SVG2-20181004/">
+                                                              <path/>
+                                                          </svg>
+                                                          <div class="card__thumb2">
+                                                              <img src="${userReviewProfile_img}" id="${i}p" alt="프로필 사진"/>
+                                                          </div>
+                                                          <div class="card__header-text">
+                                                              <h3 class="card__title">${userReviewTitle}</h3>
+                                                              <i class="far fa-thumbs-up">${userReviewLikes}</i>
+                                                              <span class="card__status">${userReviewDate}</span>
+                                                          </div>
+                                                      </div>
+                                                      <p class="card__description">${userReviewPlace}</p>
+                                                      <p class="card__description">by <b>@${userReviewNickname}</b></p>
+                                                  </div>
+                                              </a>
+                                          </li>`;
+                $("#trip_card").append(temp_html);
+              },
+            });
           }
         },
       });
@@ -99,16 +109,4 @@ function writeTrip() {
     alert("로그인이 필요한 서비스입니다.");
     window.location.href = "../templates/login.html";
   }
-}
-
-function getUser(username) {
-  $.ajax({
-    type: "GET",
-    url: `http://localhost:5081/user/${username}}`,
-    contentType: "application/json; charset=utf-8",
-    success: function (response) {
-      userReviewProfile_img = response["profileImgUrl"];
-      userReviewNickname = response["nickname"];
-    },
-  });
 }

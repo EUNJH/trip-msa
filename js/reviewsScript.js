7ac81dd9ad1e63d7b0ffd90a6f9147759bfa05b5
kim Sang Woon <alrax3574@naver.com>
2022-12-21 15:35:58 +0900

    review 8080 -> 5082
    review 8080 -> 5082

--- js/reviewsScript.js
+++ js/reviewsScript.js
@@ -2,7 +2,7 @@ function showListSort(type) {
     $('#trip_card').empty();
     $.ajax({
         type: "GET",
-        url: `http://localhost:8080/reviews?sort=${type}`,
+        url: `http://localhost:5082/reviews?sort=${type}`,
         contentType: 'application/json; charset=utf-8',
         success: function (response) {
             for (let i = 0; i < response.length; i++) {
